af5b44d8506cef2149b4d3875a90d7b34db69a23
wbrymo <wbrymo@gmail.com>
2025-06-09 21:50:32 +0100

    updated

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,8 +1,22 @@
-resource "aws_s3_bucket" "upload_bucket" {
-  bucket = "lambda-upload-bucket-${random_id.bucket_id.hex}"
-  acl    = "public-read"
-}
-
 resource "random_id" "bucket_id" {
   byte_length = 4
+}
+
+resource "aws_s3_bucket" "upload_bucket" {
+  bucket = "lambda-upload-bucket-${random_id.bucket_id.hex}"
+  acl    = "private"
+
+  tags = {
+    Name        = "Lambda Upload Bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "block_public" {
+  bucket = aws_s3_bucket.upload_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
