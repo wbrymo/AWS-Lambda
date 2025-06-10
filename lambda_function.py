67c73bda1ec2d0e85cde377cf99f58a18bbcb91a
wbrymo <111048430+wbrymo@users.noreply.github.com>
2025-06-10 15:08:37 +0100

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,12 +1,15 @@
 import json
 
 def lambda_handler(event, context):
-    print("Received event:", json.dumps(event))  # This will show up in CloudWatch
-
-    return {
+    method = event.get("requestContext", {}).get("http", {}).get("method", "UNKNOWN")
+    
+    response = {
         "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "body": json.dumps({"message": "File processed", "received": event})
+        "headers": { "Content-Type": "application/json" },
+        "body": json.dumps({
+            "message": f"File processor is active. You made a {method} request.",
+            "debug": event
+        })
     }
+    
+    return response
