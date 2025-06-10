fe7d46948dba7eae2e52c8248d5c80d56cbc74c9
wbrymo <111048430+wbrymo@users.noreply.github.com>
2025-06-10 15:30:36 +0100

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,15 +1,10 @@
 import json
 
 def lambda_handler(event, context):
-    method = event.get("requestContext", {}).get("http", {}).get("method", "UNKNOWN")
-    
-    response = {
+    return {
         "statusCode": 200,
-        "headers": { "Content-Type": "application/json" },
-        "body": json.dumps({
-            "message": f"File processor is active. You made a {method} request.",
-            "debug": event
-        })
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps({ "message": "File processed" })
     }
-    
-    return response
