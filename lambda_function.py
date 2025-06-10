b13f1278b2e794920917a60a195be3c72c054e50
wbrymo <wbrymo@gmail.com>
2025-06-10 14:15:04 +0100

    new

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,11 @@
-
 import json
 
 def lambda_handler(event, context):
     print("Event received:", json.dumps(event))
-    return {"statusCode": 200, "body": "File processed."}
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps({"message": "File processed"})
+    }
